208ab9502748e74dc4fa409d6da1699f8b71c49c
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2025-05-17 09:30:41 +0700

    feat: add new scripts and explained doc.

---
+++ bootstrap_arch.sh
@@ -0,0 +1,12 @@
+#!/usr/sbin/sh
+cd ~
+yes | pacman -Syu
+yes | pacman -S nushell helix neovim fd bat ripgrep direnv zoxide eza starship just git fzf yazi difftastic openssh gh
+git clone https://github.com/groutoutlook/nushellConfig ~/.config/nushell
+git clone https://github.com/groutoutlook/dotfiles
+ln -sr ~/dotfiles/.gitconfig ~
+ln -sr ~/dotfiles/config/helix ~/.config
+ln -sr ~/dotfiles/config/starship.toml ~/.config
+ln -sr ~/dotfiles/config/.ripgreprc ~/.config
+chsh -s /usr/bin/nu
+systemctl reboot
