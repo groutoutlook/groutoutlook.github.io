62bb27ad99c40cc4f415b3567dc787195745b6ac
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2025-05-28 00:54:58 +0700

    feat: fix some errors and moveon.

--- bootstrap_arch.sh
+++ bootstrap_arch.sh
@@ -1,12 +1,20 @@
 #!/usr/sbin/sh
 cd ~
-yes | pacman -Syu
-yes | pacman -S nushell helix neovim fd bat ripgrep direnv zoxide eza starship just git fzf yazi difftastic openssh gh
+yes | pacman -Syyuu
+yes | pacman -S nushell helix neovim fd bat ripgrep direnv zoxide eza starship just git fzf yazi difftastic openssh github-cli uutils-coreutils wl-clipboard chezmoi base-devel
 git clone https://github.com/groutoutlook/nushellConfig ~/.config/nushell
 git clone https://github.com/groutoutlook/dotfiles
 ln -sr ~/dotfiles/.gitconfig ~
+ln -sr ~/dotfiles/config/ov ~/.config
+ln -sr ~/dotfiles/config/bat ~/.config
 ln -sr ~/dotfiles/config/helix ~/.config
 ln -sr ~/dotfiles/config/starship.toml ~/.config
 ln -sr ~/dotfiles/config/.ripgreprc ~/.config
+ln -s /usr/bin/hx /usr/bin/helix
 chsh -s /usr/bin/nu
+useradd -m -G wheel -s /usr/bin/nu golk
+cp /etc/sudoers /etc/sudoers.bak
+perl -i -pe 's/^#\s*(%wheel\s+ALL=\(ALL:ALL\)\s+ALL)/$1/' /etc/sudoers
+echo "Wheel group granted sudo access."
 systemctl reboot
+
